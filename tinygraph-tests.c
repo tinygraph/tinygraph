c5aa22b05bfda33918a16ea5d96b6af1d9c6083d
daniel-j-h <daniel@trvx.org>
2021-07-24 02:04:24 +0200

    Adds vbyte encoding, see #5

--- tinygraph-tests.c
+++ tinygraph-tests.c
@@ -417,6 +417,28 @@ void test14() {
 }
 
 
+void test15() {
+  const uint32_t original[5] = {0, 256+1, 65536+1, 16777216+1, 4294967295};
+  unsigned char encoded[5 * 5];
+
+  const uint32_t n = tinygraph_vbyte_encode(original, encoded, 5);
+
+  const unsigned char expected[5 * 5] = {
+    0,
+    129, 2,
+    129, 128, 4,
+    129, 128, 128, 8,
+    255, 255, 255, 255, 15,
+  };
+
+  assert(n == 15);
+
+  for (uint32_t i = 0; i < n; ++i) {
+    assert(encoded[i] == expected[i]);
+  }
+}
+
+
 int main() {
   test1();
   test2();
@@ -432,4 +454,5 @@ int main() {
   test12();
   test13();
   test14();
+  test15();
 }
--- tinygraph-vbyte.c
+++ tinygraph-vbyte.c
@@ -1,16 +1,41 @@
 #include "tinygraph-vbyte.h"
 
 
-uint32_t tinygraph_vbyte_encode(const uint32_t *data, uint32_t *out, uint32_t n) {
-  (void)data;
-  (void)out;
-  (void)n;
+static uint32_t tinygraph_vbyte_encode_one(uint8_t *out, uint32_t value) {
+  TINYGRAPH_ASSERT(out);
 
-  return -1;
+  const uint8_t * const first = out;
+
+  for (; value >= 128; ++out) {
+    *out = (value & 127U) | 128U;
+    value = value >> 7;
+  }
+
+  *out = (uint8_t)value;
+  out += 1;
+
+  return out - first;
 }
 
 
-uint32_t tinygraph_vbyte_decode(const uint32_t *data, uint32_t *out, uint32_t n) {
+uint32_t tinygraph_vbyte_encode(const uint32_t *data, uint8_t *out, uint32_t n) {
+  TINYGRAPH_ASSERT(data);
+  TINYGRAPH_ASSERT(out);
+
+  const uint8_t * const first = out;
+
+  for (uint32_t i = 0; i < n; ++i) {
+    out += tinygraph_vbyte_encode_one(out, data[i]);
+  }
+
+  return out - first;
+}
+
+
+uint32_t tinygraph_vbyte_decode(const uint8_t *data, uint32_t *out, uint32_t n) {
+  TINYGRAPH_ASSERT(data);
+  TINYGRAPH_ASSERT(out);
+
   (void)data;
   (void)out;
   (void)n;
