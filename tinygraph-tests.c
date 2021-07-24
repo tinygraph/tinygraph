c0bd187aada790578920d4af40fc46e617e04ddd
daniel-j-h <daniel@trvx.org>
2021-07-24 16:56:43 +0200

    Adds z-order space filling curve decoding, see #17

--- tinygraph-tests.c
+++ tinygraph-tests.c
@@ -447,9 +447,18 @@ void test16() {
 
   for (uint32_t i = 0; i < 5; ++i) {
     assert(tinygraph_zorder_encode32(xs[i], ys[i]) == zs[i]);
-  }
+    assert(tinygraph_zorder_encode64(xs[i], ys[i]) == zs[i]);
 
-  // tinygraph_zorder_decode();
+    uint16_t x, y;
+    tinygraph_zorder_decode32(zs[i], &x, &y);
+    assert(x == xs[i]);
+    assert(y == ys[i]);
+
+    uint32_t xx, yy;
+    tinygraph_zorder_decode64(zs[i], &xx, &yy);
+    assert(xx == xs[i]);
+    assert(yy == ys[i]);
+  }
 }
 
 
--- tinygraph-zorder.c
+++ tinygraph-zorder.c
@@ -60,14 +60,47 @@ uint64_t tinygraph_zorder_encode64(uint32_t x, uint32_t y) {
 }
 
 
+static const uint8_t tinygraph_bitpack8[] = {
+   [0x0] = 0,   [0x1] = 1,   [0x4] = 2,   [0x5] = 3,
+  [0x10] = 4,  [0x11] = 5,  [0x14] = 6,  [0x15] = 7,
+  [0x40] = 8,  [0x41] = 9,  [0x44] = 10, [0x45] = 11,
+  [0x50] = 12, [0x51] = 13, [0x54] = 14, [0x55] = 15
+};
+
+static uint8_t tinygraph_bitpack16(uint16_t x) {
+  uint8_t v = 0;
+
+  v |= tinygraph_bitpack8[(x & 0x00ff) >> 0] << 0;
+  v |= tinygraph_bitpack8[(x & 0xff00) >> 8] << 4;
+
+  return v;
+}
+
+static uint16_t tinygraph_bitpack32(uint32_t x) {
+  uint16_t v = 0;
+
+  v |= tinygraph_bitpack16((x & 0x0000fffful) >> 0) << 0;
+  v |= tinygraph_bitpack16((x & 0xffff0000ul) >> 16) << 8;
+
+  return v;
+}
+
+static uint32_t tinygraph_bitpack64(uint64_t x) {
+  uint32_t v = 0;
+
+  v |= tinygraph_bitpack32((x & 0x00000000ffffffffull) >> 0) << 0;
+  v |= tinygraph_bitpack32((x & 0xffffffff00000000ull) >> 32) << 16;
+
+  return v;
+}
+
+
 void tinygraph_zorder_decode32(uint32_t z, uint16_t *x, uint16_t *y) {
   TINYGRAPH_ASSERT(y);
   TINYGRAPH_ASSERT(x);
 
-  (void)z;
-
-  *x = -1;
-  *y = -1;
+  *x = tinygraph_bitpack32(z & 0x55555555ul);
+  *y = tinygraph_bitpack32((z >> 1) & 0x55555555ul);
 }
 
 
@@ -75,8 +108,6 @@ void tinygraph_zorder_decode64(uint64_t z, uint32_t *x, uint32_t *y) {
   TINYGRAPH_ASSERT(y);
   TINYGRAPH_ASSERT(x);
 
-  (void)z;
-
-  *x = -1;
-  *y = -1;
+  *x = tinygraph_bitpack64(z & 0x5555555555555555ull);
+  *y = tinygraph_bitpack64((z >> 1) & 0x5555555555555555ull);
 }
