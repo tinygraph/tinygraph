cbbab2487861c5ca5b80b3fa8d356cc4735d6136
daniel-j-h <daniel@trvx.org>
2021-07-24 02:03:50 +0200

    Adds asserts to delta coding

--- tinygraph-delta.c
+++ tinygraph-delta.c
@@ -1,7 +1,11 @@
+#include "tinygraph-utils.h"
 #include "tinygraph-delta.h"
 
 
 void tinygraph_delta_encode(const uint32_t *data, uint32_t *out, uint32_t n, uint32_t prev) {
+  TINYGRAPH_ASSERT(data);
+  TINYGRAPH_ASSERT(out);
+
   for (uint32_t i = 0; i < n; ++i) {
     const uint32_t val = data[i];
 
@@ -12,6 +16,9 @@ void tinygraph_delta_encode(const uint32_t *data, uint32_t *out, uint32_t n, uin
 
 
 void tinygraph_delta_decode(const uint32_t *data, uint32_t *out, uint32_t n, uint32_t prev) {
+  TINYGRAPH_ASSERT(data);
+  TINYGRAPH_ASSERT(out);
+
   for (uint32_t i = 0; i < n; ++i) {
     const uint32_t val = data[i];
 
