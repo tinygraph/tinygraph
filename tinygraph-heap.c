e260a272b5617ecf5a98ea3f746fe4e3de4ad2a3
daniel-j-h <daniel@trvx.org>
2021-07-26 19:59:06 +0200

    Adds heap files skeletons

---
+++ tinygraph-heap.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "tinygraph-utils.h"
+#include "tinygraph-heap.h"
+
+
+typedef struct tinygraph_heap {
+  uint32_t dummy;
+} tinygraph_heap;
+
+
+tinygraph_heap* tinygraph_heap_construct() {
+  tinygraph_heap *out = malloc(sizeof(tinygraph_heap));
+
+  if (!out) {
+    return NULL;
+  }
+
+  *out = (tinygraph_heap) {
+    .dummy = -1,
+  };
+
+  return out;
+}
