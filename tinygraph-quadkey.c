6a511cbd21b817ce070eeff83a9b7f8d630cf53d
daniel-j-h <daniel@trvx.org>
2022-08-23 19:24:47 +0200

    Adds quadkey skeletons, see #28

---
+++ tinygraph-quadkey.c
@@ -0,0 +1,28 @@
+#include "tinygraph-bits.h"
+#include "tinygraph-quadkey.h"
+
+#define TINYGRAPH_R_EARTH 6378137
+#define TINYGRAPH_MIN_LAT -85.05112878
+#define TINYGRAPH_MAX_LAT 85.05112878
+#define TINYGRAPH_MIN_LNG -180
+#define TINYGRAPH_MAX_LNG 180
+
+
+uint32_t tinygraph_quadkey_get_level(uint64_t quadkey) {
+  return -1;
+}
+
+
+bool tinygraph_quadkey_has_parent(uint64_t quadkey) {
+  return false;
+}
+
+
+uint64_t tinygraph_quadkey_get_parent(uint64_t quadkey) {
+  return -1;
+}
+
+
+bool tinygraph_quadkey_is_valid(uint64_t quadkey) {
+  return false;
+}
